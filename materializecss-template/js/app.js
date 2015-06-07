453d903932007e670bec6ce0b95c5f9499f4c864
saskia <saskiavola@web.de>
2015-06-07 18:25:59 +0300

    added new folder

---
+++ materializecss-template/js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('demo', ['elasticui']);
+
+app.constant('euiHost', 'http://localhost:9200');  <!-- TODO: change to your host-->
+app.controller('IndexController', function($scope) {
+    $scope.indexName = "INDEX_NAME"; <!--TODO: change to your index-name -->
+});
+
+
